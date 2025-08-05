2d33a8aae467c69682b3d322a5db11db64014f28
Vinicius <viniciuscventura@gmail.com>
2025-08-04 21:16:19 -0300

    teste dataframe com imagens

--- utils/analise_ranking.py
+++ utils/analise_ranking.py
@@ -17,6 +17,20 @@ warnings.filterwarnings('ignore')
 
 def analise_ranking():
 
+    df_pessoal = obter_dados_pessoal()
+
+    df_pessoal['Foto'] = df_pessoal['link_foto'].str.extract(r'id=([^&]+)')  # extrai só o ID
+    df_pessoal['Foto'] = 'https://drive.google.com/thumbnail?id=' + df_pessoal['Foto']
+
+    st.dataframe(df_pessoal[["Foto", "nome_padronizado"]],
+                    column_config={
+                        'Foto': st.column_config.ImageColumn(width='medium')
+                    },
+                    column_order=['Foto', 'Nome', 'nome_padronizado', 'link_foto'],
+                    hide_index=True,
+                    use_container_width=True
+                    )
+
     col1, col2 = st.columns(2)
 
     with col1:
